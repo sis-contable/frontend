a932ca1b1c9b82f90eade93121edf1772a8ffed4
Lucia <bellomelucia@gmail.com>
2024-11-03 04:22:35 -0300

    home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,65 @@
-const home = ()=>{
-    return(
-        <div>Este es el inicio</div>
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Home = () => {
+    return (
+        <Container className="mt-4">
+        <div className="row justify-content-center">
+            <div className="col-12 col-md-6 col-lg-4 mb-4 d-flex justify-content-center">
+                <iframe 
+                    style={{
+                        maxWidth: "320px",
+                        width: "100%",
+                        height: "260px",
+                        borderRadius: "10px",
+                        boxShadow: "2px 4px 4px rgba(0, 0, 0, 0.25)",
+                        border: "1px solid #bcbcbc"
+                    }}
+                    src="https://dolarhoy.com/i/cotizaciones/dolar-blue" 
+                    frameBorder="0"
+                ></iframe>
+            </div>
+            <div className="col-12 col-md-6 col-lg-4 mb-4 d-flex justify-content-center">
+                <iframe 
+                    style={{
+                        maxWidth: "320px",
+                        width: "100%",
+                        height: "260px",
+                        borderRadius: "10px",
+                        boxShadow: "2px 4px 4px rgba(0, 0, 0, 0.25)",
+                        border: "1px solid #bcbcbc"
+                    }}
+                    src="https://dolarhoy.com/i/cotizaciones/dolar-bancos-y-casas-de-cambio" 
+                    frameBorder="0"
+                ></iframe>
+            </div>
+            <div className="col-12 col-md-6 col-lg-4 mb-4 d-flex justify-content-center">
+                <iframe 
+                    style={{
+                        maxWidth: "320px",
+                        width: "100%",
+                        height: "260px",
+                        borderRadius: "10px",
+                        boxShadow: "2px 4px 4px rgba(0, 0, 0, 0.25)",
+                        border: "1px solid #bcbcbc"
+                    }}
+                    src="https://dolarhoy.com/i/cotizaciones/dolar-mep" 
+                    frameBorder="0"
+                ></iframe>
+            </div>
+        </div>
+        <div className='row'>
+            <div style={{ minHeight: '600px', width: '100%' }}>
+                <iframe
+                    src="https://www.lanacion.com.ar/economia/"
+                    style={{ width: '100%', height: '100%' }}
+                    frameBorder="0"
+                    title="La Nación Economía"
+                ></iframe>
+            </div>
+        </div>
+    </Container>  
     );
 }
 
-export default home;
-//Buscar despues react router que sirve para manejar las rutas/componentes
+export default Home;
